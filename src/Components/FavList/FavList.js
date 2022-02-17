0ab3adc0c825383c69ba91304bf4398c00447fec
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-02-17 11:48:31 +0200

    latest

--- src/Components/FavList/FavList.js
+++ src/Components/FavList/FavList.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import FavListItems from '../FavListitems/FavListitems.js'
+import './favList.css'
 const FavList = (props) => {
     console.log(props);
   return (
     <>
      <h1>This is my favorite list of Movies</h1>
-     <main>
+     <main className = "fav">
          <FavListItems favoriteList={props.favoriteList} />
      </main>
 
